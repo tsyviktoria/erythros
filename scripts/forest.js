d6e586dbb44086c1674d9fda0975eaa55ed516a2
tsyviktoria <vitusya.l90@gmail.com>
2026-05-10 20:56:35 +0300

    updated script and style for forest location

--- scripts/forest.js
+++ scripts/forest.js
@@ -101,10 +101,10 @@ function handleHeadphones(choice) {
 continueBtn.addEventListener('click', () => {
     if (!playerHasHeadphones) {
         // Логіка для Векни (перезавантаження)
-        window.location.href = "/upsidedown.html"
+        window.location.href = "upsidedown.html"
     } else {
         //alert("Ви прямуєте глибше в ліс під звуки Running Up That Hill...");
         // Тут перехід до наступної сцени
-        window.location.href = "/castle.html"
+        window.location.href = "mansion.html"
     }
 });
